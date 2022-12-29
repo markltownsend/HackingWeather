f80e427f87bd1644ab53d4ada69407fb2db66386
Mark Townsend <markltownsend@users.noreply.github.com>
2022-12-29 07:21:02 -0800

    Initial Commit

---
+++ HackingWeather/HackingWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWeatherApp.swift
+//  HackingWeather
+//
+//  Created by Mark Townsend on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct HackingWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
